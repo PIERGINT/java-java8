afb2048ca3601381cbf881bd19e7f87f93f8d0bc
PIERGIT <p.marget@yahoo.com>
2019-11-29 14:32:10 +0100

    TP1 - DATE et HEURE -incomplet 29 11 19

--- src/test/java/java8/ex01/DateAndTime_01_Test.java
+++ src/test/java/java8/ex01/DateAndTime_01_Test.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.time.ZoneId;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -18,11 +19,13 @@ public class DateAndTime_01_Test {
     public void test_date() {
 
         // TODO modifier les paramètres pour que le test soit passant
-        Date date = new Date(0, 0, 0, 0,0,0);
+        @SuppressWarnings("deprecation")
+		Date date = new Date(117,2, 24, 01,02,03);
 
         assertThat(date.toString(), is("Fri Mar 24 01:02:03 CET 2017"));
 
         // une date est mutable... :-(
+        date.setDate(25);
         // TODO modifier le jour de la date pour que le test soit passant
 
         assertThat(date.toString(), is("Sat Mar 25 01:02:03 CET 2017"));
@@ -33,12 +36,25 @@ public class DateAndTime_01_Test {
     @Test
     public void test_calendar() throws Exception {
         Calendar calendar = Calendar.getInstance();
+       calendar.set(Calendar.DAY_OF_WEEK, 4);
+       calendar.set(Calendar.MONTH, 3);
+       calendar.set(Calendar.DAY_OF_MONTH, 24);
+       calendar.set(Calendar.HOUR_OF_DAY, 01);
+       calendar.set(Calendar.MINUTE, 02);
+       calendar.set(Calendar.SECOND, 03);
+       calendar.set(Calendar.YEAR, 2017);
+       
+       
+       
+       
 
         // TODO modifier l'objet calendar pour que le test soit passant
 
         assertThat(calendar.getTime().toString(), is("Fri Mar 24 01:02:03 CET 2017"));
 
         // calendar est mutable... :-(
+        calendar.set(Calendar.DAY_OF_WEEK, 5);
+        calendar.set(Calendar.DAY_OF_MONTH, 25);
         // TODO modifier l'objet calendar pour que le test soit passant
 
         assertThat(calendar.getTime().toString(), is("Sat Mar 25 01:02:03 CET 2017"));
